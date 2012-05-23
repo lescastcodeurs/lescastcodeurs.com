d960f6421e62dd4b46b5297890949f9a797cd67d
Emmanuel Bernard <emmanuel@hibernate.org>
2012-05-23 22:10:06 +0200

    Add blog module

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,7 +1,17 @@
+require 'tagger_sanitizer'
+require 'atomizer_custom'
+
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::DataDir.new
-  # extension Awestruct::Extensions::Posts.new( '/news' ) 
+  extension Awestruct::Extensions::Posts.new('', :posts)
+  extension Awestruct::Extensions::Paginator.new( :posts, '/index', :per_page=>10 )
   extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Atomizer.new( 
+    :posts, 
+    '/feed', 
+    :num_entries=>10000,
+    :content_url=>'http://lescastcodeurs.com',
+    :feed_title=> 'Les Cast Codeurs Podcast' )
   extension Awestruct::Extensions::Flattr.new
   extension Awestruct::Extensions::Disqus.new
   helper Awestruct::Extensions::GoogleAnalytics
